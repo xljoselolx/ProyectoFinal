d477a7f724cee9d71e0b5a076db3e77812e49815
Jose Luis Alcon <josegurka@gmail.com>
2021-10-26 08:09:34 -0400

    actualizacion del proyecto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CELUCENTRUM</title>
+    <link href = "https://fonts.googleapis.com/css?family=Roboto"rel =" stylesheet ">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+<body>
+    <h1>Bienvenido a Celucentrum </h1>
+    <h2>Por Inicie sesion o Registrese</h2>
+    <a href="login.php">Login</a> or 
+    <a href="registro.php">Registro</a> 
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Login</title>
+
+    <link href = "https://fonts.googleapis.com/css?family=Roboto"rel =" stylesheet ">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="login.php" method="POST">
+      <input name="email" type="text" placeholder="Enter your email">
+      <input name="password" type="password" placeholder="Enter your Password">
+      <input type="submit" value="Submit">
+    </form>
+
+
+
+</body>
+</html>
